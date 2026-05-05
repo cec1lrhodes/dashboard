2ff1cf8330b4787da95550df37e4128fc831ff65
cec1lrhodes <kubalewand21@gmail.com>
2026-05-05 11:28:40 +0300

    events card

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import vector from "./screenshots/vector.png";
 import searchicon from "./screenshots/searchicon.png";
 import group from "./screenshots/group.png";
 import staravatar from "./screenshots/staravatar.png";
+import arrowright from "./screenshots/arrowright.png";
 
 import { BarChart, Bar, ResponsiveContainer, LineChart, Line } from "recharts";
 
@@ -34,6 +35,12 @@ const earningsActivityData = [
   { value: 7 },
   { value: 9 },
   { value: 10 },
+];
+
+const events = [
+  { title: "Meet w/ Simmmple", time: "01:00 PM - 02:00 PM" },
+  { title: "Fitness Training", time: "02:00 PM - 03:00 PM" },
+  { title: "Reading Time", time: "03:00 PM - 04:00 PM" },
 ];
 
 function App() {
@@ -355,7 +362,39 @@ function App() {
         <section className="flex gap-5 px-[31px] mt-5 ">
           <div className={styles.smallCard}></div>
           <div className={styles.smallCard}></div>
-          <div className={styles.smallCard}></div>
+          <div className={styles.smallCard + " relative"}>
+            <div>
+              <p className="text-[#1B2559] px-7 text-[34px] mt-5 whitespace-nowrap font-dm-sans font-bold leading-[42px] tracking-[-0.02em]">
+                27 May
+              </p>
+            </div>
+
+            {events.map((item, i) => (
+              <div
+                key={i}
+                className="flex items-center gap-[10px] mt-5 pl-[28px]"
+              >
+                <div className="w-[4px] h-[41px] bg-[#4318FF] rounded-full shrink-0" />
+                <div>
+                  <p className="text-[#1B2559] text-[16px] whitespace-nowrap font-dm-sans font-bold leading-[28px] tracking-[-0.02em]">
+                    {item.title}
+                  </p>
+                  <p className="text-[#A3AED0] text-[12px] whitespace-nowrap font-dm-sans font-medium leading-[20px] tracking-[-0.02em]">
+                    {item.time}
+                  </p>
+                </div>
+              </div>
+            ))}
+
+            <div className=" flex items-center gap-[5px] absolute bottom-[22px] right-[51px] text-[#4318FF] text-[16px] font-dm-sans font-bold leading-[28px] tracking-[-0.02em] cursor-pointer">
+              View all Tasks
+              <img
+                src={arrowright}
+                alt="arrowright"
+                className="w-[18px] h-[12px]"
+              />
+            </div>
+          </div>
         </section>
       </main>
     </div>
